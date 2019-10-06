7e8af087567c6c86157258bc99b2384e6fa5825e
ilhamdzik11 <ilhamdzik11@gmail.com>
2019-10-06 12:33:09 +0700

    add about view

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('foo', function () {
-    return 'Hello World';
+Route::get('/about', function () {
+    return view ('about');
 });
